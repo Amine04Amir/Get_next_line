940125c3aea68276364dc75ee566b12650db23d7
Mohamed Amine Amir <mamir@e1r10p5.1337.ma>
2024-02-13 16:04:52 +0100

    GNL

--- get_next_line.c
+++ get_next_line.c
@@ -12,7 +12,7 @@
 
 #include "get_next_line.h"
 
-char	*ft_rest(char *str)
+char	*ft_rest(char *statik)
 {
 	int		i;
 	int		j;
@@ -20,54 +20,54 @@ char	*ft_rest(char *str)
 
 	i = 0;
 	j = 0;
-	if (!str)
+	if (!statik)
 		return (NULL);
-	while (str[i] != '\n' && str[i])
+	while (statik[i] != '\n' && statik[i])
 		i++;
-	if (str[i] == '\0')
+	if (statik[i] == '\0')
 	{
-		free(str);
+		free(statik);
 		return (NULL);
 	}
-	line = malloc(ft_strlen(str) - i + 1);
+	line = malloc(ft_strlen(statik) - i + 1);
 	if (!line)
 		return (NULL);
 	i++;
-	while (str[i])
-		line[j++] = str[i++];
+	while (statik[i])
+		line[j++] = statik[i++];
 	line[j] = '\0';
-	free(str);
+	free(statik);
 	return (line);
 }
 
-char	*ft_read_line(char *str)
+char	*ft_read_line(char *statik)
 {
 	int		i;
 	char	*line;
 
 	i = 0;
-	if (!str[i])
+	if (!statik[i])
 		return (NULL);
-	while (str[i] != '\n' && str[i] != '\0')
+	while (statik[i] != '\n' && statik[i] != '\0')
 		i++;
-	if (str[i] == '\n')
+	if (statik[i] == '\n')
 		i++;
 	line = (char *)malloc(i + 1);
 	if (!line)
 		return (NULL);
 	i = 0;
-	while (str[i] != '\n' && str[i] != '\0')
+	while (statik[i] != '\n' && statik[i] != '\0')
 	{
-		line[i] = str[i];
+		line[i] = statik[i];
 		i++;
 	}
-	if (str[i] == '\n')
+	if (statik[i] == '\n')
 		line[i++] = '\n';
 	line[i] = '\0';
 	return (line);
 }
 
-char	*ft_read(int fd, char *line)
+char	*ft_read(int fd, char *statik)
 {
 	char	*buffer;
 	int		bytes_read;
@@ -76,20 +76,20 @@ char	*ft_read(int fd, char *line)
 	buffer = malloc((size_t)BUFFER_SIZE + 1);
 	if (!buffer)
 		return (NULL);
-	while (bytes_read != 0 && !ft_strchr(line, '\n'))
+	while (bytes_read != 0 && !ft_strchr(statik, '\n'))
 	{
 		bytes_read = read(fd, buffer, BUFFER_SIZE);
 		if (bytes_read < 0)
 		{
 			free(buffer);
-			free(line);
+			free(statik);
 			return (NULL);
 		}
 		buffer[bytes_read] = '\0';
-		line = ft_strjoin(line, buffer);
+		statik = ft_strjoin(statik, buffer);
 	}
 	free(buffer);
-	return (line);
+	return (statik);
 }
 
 char	*get_next_line(int fd)
